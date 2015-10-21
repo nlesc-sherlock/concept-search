8bfdcc0ef8567245785a2b6a80816f6fc460e76e
lars <l.ridder@esciencecenter.nl>
2015-10-21 12:53:51 +0200

    Added elastic search based term suggester

---
+++ relate/elsearch.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# run ./bin/elasticsearch in the directory where you installed it
+
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+def create_index(dump_dir):
+    config = {}
+    config['settings'] = {
+        'analysis' : {
+        'analyzer': {
+            'default': {
+                'type':'standard',
+                'stopwords': '_english_',
+            }
+        }
+        }
+    }
+
+    config['mappings'] = { 
+        'email': {
+        'properties': {
+            'text': {
+                'type': 'string', 
+                'term_vector': 'with_positions_offsets_payloads'
+            },
+        }
+        }
+    }
+    es.indices.create(index='enron', body=config)
+
+    # index data
+    for p in os.listdir(dump_dir):
+        with open(os.path.join(dump_dir, p), 'r') as f:
+            data = f.read()
+        
+        es.bulk(index='enron', doc_type='email', body=data, request_timeout=20)
+
+def suggest_terms(query_word):
+    query = {
+        "query": {
+            "query_string": {
+                "query": query_word
+            }
+        },
+        "aggregations" : {
+            "significantTerms" : {
+                "significant_terms" : { "field" : "text", "size": 15 }
+            }
+        }
+    }
+    res = es.search(index='enron', doc_type='email', body=query, size=0)
+    term_dict={}
+    for b in res['aggregations']['significantTerms']['buckets']:
+        term_dict[b['key']] = b['score']
+    return term_dict
