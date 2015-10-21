f13edc6bc91e5d908bdc6dc0878849d4032e0bd1
lars <l.ridder@esciencecenter.nl>
2015-10-21 14:37:32 +0200

    Created ELSearch class

--- relate/elsearch.py
+++ relate/elsearch.py
@@ -4,54 +4,58 @@
 
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
+import os
 
-def create_index(dump_dir):
-    config = {}
-    config['settings'] = {
-        'analysis' : {
-        'analyzer': {
-            'default': {
-                'type':'standard',
-                'stopwords': '_english_',
+class ELSearch():
+    """Class to perform searches with Elastic Search"""
+    def __init__(self,dump_dir=None, initialize=False):
+        if initialize:
+            config = {}
+            config['settings'] = {
+                'analysis' : {
+                'analyzer': {
+                    'default': {
+                        'type':'standard',
+                        'stopwords': '_english_',
+                    }
+                }
+                }
             }
-        }
-        }
-    }
 
-    config['mappings'] = { 
-        'email': {
-        'properties': {
-            'text': {
-                'type': 'string', 
-                'term_vector': 'with_positions_offsets_payloads'
+            config['mappings'] = { 
+                'email': {
+                'properties': {
+                    'text': {
+                        'type': 'string', 
+                        'term_vector': 'with_positions_offsets_payloads'
+                    },
+                }
+                }
+            }
+            es.indices.create(index='enron', body=config)
+
+            # index data
+            for p in os.listdir(dump_dir):
+                with open(os.path.join(dump_dir, p), 'r') as f:
+                    data = f.read()
+
+                es.bulk(index='enron', doc_type='email', body=data, request_timeout=20)
+
+    def suggest_terms(self,query_word):
+        query = {
+            "query": {
+                "query_string": {
+                    "query": query_word
+                }
             },
-        }
-        }
-    }
-    es.indices.create(index='enron', body=config)
-
-    # index data
-    for p in os.listdir(dump_dir):
-        with open(os.path.join(dump_dir, p), 'r') as f:
-            data = f.read()
-        
-        es.bulk(index='enron', doc_type='email', body=data, request_timeout=20)
-
-def suggest_terms(query_word):
-    query = {
-        "query": {
-            "query_string": {
-                "query": query_word
-            }
-        },
-        "aggregations" : {
-            "significantTerms" : {
-                "significant_terms" : { "field" : "text", "size": 15 }
+            "aggregations" : {
+                "significantTerms" : {
+                    "significant_terms" : { "field" : "text", "size": 15 }
+                }
             }
         }
-    }
-    res = es.search(index='enron', doc_type='email', body=query, size=0)
-    term_dict={}
-    for b in res['aggregations']['significantTerms']['buckets']:
-        term_dict[b['key']] = b['score']
-    return term_dict
+        res = es.search(index='enron', doc_type='email', body=query, size=0)
+        term_dict={}
+        for b in res['aggregations']['significantTerms']['buckets']:
+            term_dict[b['key']] = b['score']
+        return term_dict
