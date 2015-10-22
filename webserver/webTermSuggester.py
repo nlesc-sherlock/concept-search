4792e5eb90bb4f76782bf26be319e3f31e192f52
Oscar Martinez Rubi <omrubi@gmail.com>
2015-10-22 11:58:18 +0200

    Add error handling, possibility to specify methods and aggregation

--- webserver/webTermSuggester.py
+++ webserver/webTermSuggester.py
@@ -3,10 +3,11 @@
 #    Created by Oscar Martinez                                                 #
 #    o.rubi@esciencecenter.nl                                                  #
 ################################################################################
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, jsonify
 from TermSuggester import TermSuggester, SearchMethodAggregation
 from elsearch import ELSearch
 from wnsearch import WNSearch
+import MakeChart
 
 app = Flask(__name__)
 
@@ -14,16 +15,50 @@ searchMethodClasses = (ELSearch, WNSearch)
 initializeParameters = ((None, False),([]))
 ts = TermSuggester(searchMethodClasses, initializeParameters)
 
+@app.route('/')
+def api_root():
+    return 'Welcome to TermSuggester Web API'
+
+@app.errorhandler(404)
+def api_error(error=None):
+    message = {
+            'status': 404,
+            'message': 'Error: ' + error,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
 @app.route("/suggester", methods = ['GET',])
 def api_term():
     if request.method == 'GET':
         if 'term' in request.args:
-            data = ts.getSuggestions(str(request.args['term']), SearchMethodAggregation.SumMethod)
-            resp = jsonify(data)
-            resp.status_code = 200
+            if 'agg-method' in request.args:
+                aggMethod = str(request.args['agg-method']).strip()
+                if aggMethod == 'sum':
+                    aggMethod = SearchMethodAggregation.SumMethod
+                elif aggMethod == 'average':
+                    aggMethod = SearchMethodAggregation.AverageMethod
+                else:
+                    return api_error('specify correct aggregation method: sum or average')
+            else:
+                # Default aggragation method
+                aggMethod = SearchMethodAggregation.SumMethod
+            
+            if 'methods' in request.args:
+                try:
+                    methods = str(request.args['methods']).split(',')
+                    for i in range(len(methods)):
+                        methods[i] = int(methods[i])
+                except:
+                    return api_error('specify correct method. Example: 1,2')
+            else:
+                methods = None
+            data = ts.getSuggestions(str(request.args['term']), aggMethod, methods)
+            resp = Response(MakeChart.dict2bar(data), status=200, mimetype='application/json')
             return resp
         else:
-            return 'Error: Need to specif a term!'
+            return api_error('a term is required')
 
 
 
