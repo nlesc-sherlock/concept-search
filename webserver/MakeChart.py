18168b86f405fcfe16a4046009a255977a0c2cda
lars <l.ridder@esciencecenter.nl>
2015-10-22 16:16:42 +0200

    rotated axis labels

--- webserver/MakeChart.py
+++ webserver/MakeChart.py
@@ -1,4 +1,5 @@
 import vincent,pandas,operator
+from vincent import AxisProperties, PropertySet, ValueRef
 
 def dict2bar(d):
     # function to convert a dictionary created by TermSuggester to vega readable json
@@ -7,5 +8,13 @@ def dict2bar(d):
     v = [x[1] for x in sortedlist]
     df = pandas.DataFrame(v,i)
     bar = vincent.Bar(df)
+    vert2horiz(bar)
     bar.axis_titles(x='Terms', y='Scores')
+    ax = AxisProperties(labels = PropertySet(angle=ValueRef(value=-90)))
+    bar.axes[0].properties=ax
+    bar.axes[0].properties
+    bar.height=350
     return bar.to_json()
+    
+def vert2horiz(bar):
+    print bar.grammar()
