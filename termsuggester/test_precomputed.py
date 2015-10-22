6f6b6d8b1aa8658d5e7493fab244e15d00a21cb9
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-10-22 16:40:54 +0200

    bug: precomputed's suggest_terms should return dict, not list

--- termsuggester/test_precomputed.py
+++ termsuggester/test_precomputed.py
@@ -6,9 +6,10 @@ from precomputed import PrecomputedSuggester
 class TestPrecomputed(TestCase):
     def test_precomputed(self):
         suggest = PrecomputedSuggester().suggest_terms
-        terms = [term for term, _ in suggest('baseball')]
+        self.assertTrue(isinstance(suggest('baseball'), dict))
+        terms = [term for term in suggest('baseball')]
         self.assertIn('ticket', terms)
 
-        terms = [term for term, _ in suggest('import')]
+        terms = [term for term in suggest('import')]
         self.assertIn('export', terms)
         self.assertIn('import', terms)
