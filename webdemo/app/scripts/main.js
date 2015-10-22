896bcb209cf3f638695da9f3b4b51ccdad8aeca6
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-10-22 13:22:48 +0200

    Hook up webserver in webdemo

--- webdemo/app/scripts/main.js
+++ webdemo/app/scripts/main.js
@@ -1,10 +1,34 @@
-$.getJSON( "http://localhost:9000/data/vega.json", function( data ) {
-  console.log('got the data!');
-  var spec = data;
+$("#search").submit(function(e) {
+  console.log('form submitted');
+  e.preventDefault();
 
-  vg.parse.spec(spec, function(chart) {
-  var view = chart({el:".chart"})
-    .update();
+  $.ajax({
+    dataType: "json",
+    url: "http://127.0.0.1:5000/suggester",
+    method: "get",
+    data: $("#search").serialize(),
+    xhrFields: {
+      withCredentials: true
+    },
+    success: function(data) {
+      console.log('got the data!');
+      console.log(data);
+
+      displayChart(data);
+
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status);
+        console.log(thrownError);
+      }
   });
-
 });
+
+function displayChart(json_data) {
+  vg.parse.spec(json_data, function(chart) {
+    var view = chart({
+        el: ".chart"
+      })
+      .update();
+  });
+}
