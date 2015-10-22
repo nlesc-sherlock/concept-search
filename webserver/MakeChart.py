58b99ac9f1b51efa66e1b78467175c1f23a918be
lars <l.ridder@esciencecenter.nl>
2015-10-22 11:42:11 +0200

    Added labels to bars

--- webserver/MakeChart.py
+++ webserver/MakeChart.py
@@ -1,6 +1,8 @@
-import vincent
+import vincent,pandas
 
 def dict2bar(d):
     # function to convert a dictionary created by TermSuggester to vega readable json
-    bar = vincent.Bar(d.values())
+    df = pandas.DataFrame(d.values(),index = d.keys())
+    bar = vincent.Bar(df)
+    bar.axis_titles(x='Terms', y='Scores')
     return bar.to_json()
