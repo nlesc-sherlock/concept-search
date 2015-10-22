569f390793d1dc197014a9342a8f7de62b787efd
Oscar Martinez Rubi <omrubi@gmail.com>
2015-10-22 10:58:23 +0200

    Add web server to serve term suggestions

---
+++ webserver/webTermSuggester.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+################################################################################
+#    Created by Oscar Martinez                                                 #
+#    o.rubi@esciencecenter.nl                                                  #
+################################################################################
+from flask import Flask, request, jsonify
+from TermSuggester import TermSuggester, SearchMethodAggregation
+from elsearch import ELSearch
+from wnsearch import WNSearch
+
+app = Flask(__name__)
+
+searchMethodClasses = (ELSearch, WNSearch)
+initializeParameters = ((None, False),('/home/oscarr/concept-search-wd/data/wordnet', False))
+ts = TermSuggester(searchMethodClasses, initializeParameters)
+
+@app.route("/suggester", methods = ['GET',])
+def api_term():
+    if request.method == 'GET':
+        if 'term' in request.args:
+            data = ts.getSuggestions(str(request.args['term']), SearchMethodAggregation.SumMethod)
+            resp = jsonify(data)
+            resp.status_code = 200
+            return resp
+        else:
+            return 'Error: Need to specif a term!'
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
