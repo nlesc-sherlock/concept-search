7b073caf90d663a333bd270442d8d35c841c295c
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-10-22 14:27:02 +0200

    add explicit index and doc_type in PrecomputedSuggester

--- relate/precomputed.py
+++ relate/precomputed.py
@@ -9,7 +9,8 @@ class PrecomputedSuggester(object):
 
     def suggest_terms(self, query_word):
         terms = set()
-        for hit in scan(self._es, query=_make_query(query_word)):
+        for hit in scan(self._es, query=_make_query(query_word),
+                        index='suggestions', doc_type='cluster'):
             terms.update(hit['_source']['terms'].split())
         # We should keep the weights around, but currently we don't.
         return [(term, 1.0) for term in terms]
