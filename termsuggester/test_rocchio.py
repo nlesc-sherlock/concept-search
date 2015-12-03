fdc7819aca2bce1a7a2f63a0cd37a4e85a3178bb
Oscar Martinez Rubi <omrubi@gmail.com>
2015-12-03 17:49:04 +0100

    add test_rocchio

--- termsuggester/test_rocchio.py
+++ termsuggester/test_rocchio.py
@@ -4,9 +4,10 @@ from rocchio import RocchioSuggester
 
 class TestRocchio(unittest.TestCase):
     def test_rocchio(self):
-        suggest = RocchioSuggester().suggest_terms
-        termsDict = suggest('baseball')
+        suggester = RocchioSuggester(numResults = 30, numTop = 20, numBottom = 20, weightInitial = 1., weightTop = 1., weightBottom = 1.)
+        termsDict = suggester.suggest_terms('baseball')
         print termsDict
+        print suggester.__class__.__name__
         self.assertTrue(isinstance(termsDict, dict))
         self.assertIn('yahoo', termsDict.keys())
 
